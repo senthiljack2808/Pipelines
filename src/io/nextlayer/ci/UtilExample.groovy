568ac34ac71d6edf388ac9192f4089703b3ef4c1
Senthil Kumar <senthil@striim.com>
2024-03-26 10:22:34 +0530

    date time format

--- src/io/nextlayer/ci/UtilExample.groovy
+++ src/io/nextlayer/ci/UtilExample.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
 package io.nextlayer.ci
 
-import org.example.App
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 class UtilExample implements Serializable {
 
@@ -9,7 +10,17 @@ class UtilExample implements Serializable {
     }
     
     String doSomething() {
-        new App().run();
+        // Get the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now()
+
+        // Define a custom date-time format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+        // Format the current date and time using the formatter
+        String formattedDateTime = currentDateTime.format(formatter)
+
+        // Print the formatted date and time
+        println "Formatted Date and Time: $formattedDateTime"
         return "Something done"
     }
 }
