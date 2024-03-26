bbf29e120f28d326873a74abddaebc7b08f94ac3
Senthil Kumar <senthil@striim.com>
2024-03-26 11:41:35 +0530

    run method in App class

---
+++ src/io/nextlayer/ci/App.java
@@ -0,0 +1,14 @@
+package io.nextlayer.ci
+
+public class App {
+    public App() {
+    }
+
+    public void run() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- src/io/nextlayer/ci/UtilExample.groovy
+++ src/io/nextlayer/ci/UtilExample.groovy
@@ -1,11 +1,10 @@
 #!/usr/bin/env groovy
 package io.nextlayer.ci
 
-
+import io.nextlayer.ci.App;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.apache.commons.lang3.StringUtils;
-import org.example.App;
 
 class UtilExample implements Serializable {
 
