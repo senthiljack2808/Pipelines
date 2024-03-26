88c6a095ecc3e8ecf848760593b340f3cedd6e87
Senthil Kumar <senthil@striim.com>
2024-03-26 12:53:00 +0530

    remove few dependencies in pom

---
+++ src/io/nextlayer/ci/A.java
@@ -0,0 +1,7 @@
+package io.nextlayer.ci;
+public class A {
+
+ void sayHello() {
+        System.out.println("Hello JAVA--");
+}
+        }
--- src/io/nextlayer/ci/App.groovy
+++ src/io/nextlayer/ci/App.groovy
@@ -1,10 +1,12 @@
 package io.nextlayer.ci
+import io.nextlayer.ci.A;
 
 public class App {
     public App() {
     }
 
      String run() {
+         new A().sayHello();
         return "Hello world!"
     }
 
