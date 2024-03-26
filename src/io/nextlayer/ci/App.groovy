887c2427bf15a598427565cbc859463a1179eb36
Senthil Kumar <senthil@striim.com>
2024-03-26 11:58:35 +0530

    Hello world program

--- src/io/nextlayer/ci/App.groovy
+++ src/io/nextlayer/ci/App.groovy
@@ -1,5 +1,4 @@
 package io.nextlayer.ci
-import static org.junit.Assert.assertEquals
 
 public class App {
     public App() {
@@ -7,7 +6,6 @@ public class App {
 
     public void run() {
         println "Hello world!"
-        assertEquals(1, 1)
     }
 
     public static void main(String[] args) {
