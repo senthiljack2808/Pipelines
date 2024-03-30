7fb135c05479747138a16bed58aa9c6076f5bc15
Senthil Kumar <senthil@striim.com>
2024-03-30 17:04:38 +0530

    output

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,8 +4,8 @@ def call(String name = "human") {
      UtilExample util = new UtilExample()
     String imageName =util.doSomething()
     String helloJava=util.helloJava()
-    def output = sh(script: "Hello : ${util.helloJava()}")
-    echo "output : ${output}"
+    //def output = sh(script: "Hello : ${util.helloJava()}")
+    echo "output : ${sh(script: util.helloJava(), returnStdout: true)}"
     echo "Something : ${imageName}"
     echo "Hello, ${name}."
    
